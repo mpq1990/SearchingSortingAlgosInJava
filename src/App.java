6c9f9d3d75842c611c20a9f2637ea92524310fdc
Majid Qureshi <mpq1990@gmail.com>
2017-10-22 22:39:40 +0200

    recursive linear search

--- src/App.java
+++ src/App.java
@@ -5,9 +5,12 @@ public class App {
 		int[] array = {1, 3,5,6 ,7 ,67,777};
 		int index = binarySearch(7775, array);
 		System.out.println(index);
+//		reduceByOne(10);
+		index = recursiveLinearSearch(array, 0, 67);
+		System.out.println(index);
 	}
 	
-	private static int binarySearch(int key, int[] array){
+	private static int binarySearch(int key, int[] array) {
 		int p = 0;
 		int r = array.length - 1;
 		int q = 0;
@@ -22,5 +25,23 @@ public class App {
 			}
 		}
 		return -1;
+	}
+	
+	public static void reduceByOne(int n){
+		if( n != 0) {
+			reduceByOne(n - 1);
+		}
+		System.out.println(n);
+	}
+	
+	public static int recursiveLinearSearch(int[] a ,  int i, int x) {
+		if (i > a.length - 1){
+			return -1;
+		} else if (a[i] == x){
+			return i;
+		} else {
+			System.out.println(a + " aa " + i + " " + a[i] + " aa " + x );
+			return recursiveLinearSearch(a, i+1, x);
+		}
 	}
 }
