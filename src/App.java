88b10f907a397f7395d397500a34b0c0f991b2b2
Majid Qureshi <mpq1990@gmail.com>
2017-10-23 23:45:25 +0200

    added insertion sort

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		int[] array = {1, 3,5,6 ,7 ,67,777};
 		int[] unsortedArray = {33, 2, 431, 23423, 3242,12,1,43};
-		
+		int[] unsortedArrayTwo = {33, 2, 431, 23423, 3242,12,1,43};
 		int index = binarySearch(7775, array);
 		System.out.println(index);
 		
@@ -17,10 +17,9 @@ public class App {
 		System.out.println(index);
 		
 		int[] sorted = selectionsort(unsortedArray);
-		
-		for(int i = 0 ; i < sorted.length; i++){
-			System.out.print(sorted[i] + ",");
-		}
+		printArray(sorted);
+		int[] sortedArrayTwo = insertionSort(unsortedArrayTwo);
+		printArray(sortedArrayTwo);
 	}
 	
 	private static int binarySearch(int key, int[] array) {
@@ -89,5 +88,28 @@ public class App {
 		}
 		
 		return sortedArray;
+	}
+	
+	public static int [] insertionSort(int [] unsortedArray){
+		int [] sortedArray = unsortedArray;
+		
+		for(int i = 1; i < unsortedArray.length; i++) {
+			int j = i;
+			while(j > 0 && unsortedArray[j - 1] > unsortedArray[j]){
+				int temp = unsortedArray[j];
+				unsortedArray[j] = unsortedArray[j - 1];
+				unsortedArray[j - 1] = temp;
+				j--;
+			}
+		}
+		
+		return sortedArray;
+	}
+	
+	public static void printArray(int[] array){
+		for(int i = 0 ; i < array.length; i++){
+			System.out.print(array[i] + ",");
+		}
+		System.out.println();
 	}
 }
