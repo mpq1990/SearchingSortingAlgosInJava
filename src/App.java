fdf005d89efe91acde8f6e077953dccb446603ab
Majid Qureshi <mpq1990@gmail.com>
2017-11-05 13:22:01 +0100

    implemented merge sort algorithm

--- src/App.java
+++ src/App.java
@@ -5,6 +5,9 @@ public class App {
 		int[] array = {1, 3,5,6 ,7 ,67,777};
 		int[] unsortedArray = {33, 2, 431, 23423, 3242,12,1,43};
 		int[] unsortedArrayTwo = {33, 2, 431, 23423, 3242,12,1,43};
+		int[] unsortedArrayThree = {33, 2, 431, 23423, 3242,12,1,43,5,5,5,5,5,5,5,65};
+		
+		
 		int index = binarySearch(7775, array);
 		System.out.println(index);
 		
@@ -20,6 +23,10 @@ public class App {
 		printArray(sorted);
 		int[] sortedArrayTwo = insertionSort(unsortedArrayTwo);
 		printArray(sortedArrayTwo);
+		
+		mergeSort(unsortedArrayThree, 0, unsortedArrayThree.length - 1);
+		printArray(unsortedArrayThree);
+		
 	}
 	
 	private static int binarySearch(int key, int[] array) {
@@ -105,6 +112,66 @@ public class App {
 		
 		return sortedArray;
 	}
+	
+	/**
+	 * start merge sort
+	 * @param inputArray
+	 * @param start
+	 * @param end
+	 */
+	
+	public static void mergeSort(int inputArray[], int start, int end){
+		if(end <= start){
+			return;
+		}
+		int mid = (start + end) / 2;
+		mergeSort(inputArray, start, mid);
+		mergeSort(inputArray, mid + 1, end);
+		merge(inputArray, start, mid, end);
+	}
+	
+	public static void merge(int inputArray[], int start, int mid, int end){
+		int tempArray[] = new int[end - start + 1];
+		
+		int leftSlot = start;
+		int rightSlot = mid + 1;
+		
+		int  k = 0;
+		
+		while (leftSlot <= mid && rightSlot <= end){
+			if(inputArray[leftSlot] < inputArray[rightSlot]){
+				tempArray[k] = inputArray[leftSlot];
+				leftSlot++;
+			} else{
+				tempArray[k] = inputArray[rightSlot];
+				rightSlot++;
+			}
+			k++;
+		}
+		
+		// above loop completed and right and left side have been sorted
+		if(leftSlot <= mid){ // consider the right side done being sorted... left must be stored..
+			while(leftSlot <= mid){
+				tempArray[k] = inputArray[leftSlot];
+				leftSlot++;
+				k++;
+			}
+		} else if(rightSlot <= end) { // left side has been sorted and right must be stored
+			while(rightSlot <= end){
+				tempArray[k] = inputArray[rightSlot];
+				rightSlot++;
+				k++;
+			}
+		}
+		
+		//copy over the temp array into the appropraite slot
+		for(int i =0; i < tempArray.length; i++ ){
+			inputArray[start + i] = tempArray[i];
+		}
+		
+	}
+	//******************************End merge sort
+	
 	
 	public static void printArray(int[] array){
 		for(int i = 0 ; i < array.length; i++){
